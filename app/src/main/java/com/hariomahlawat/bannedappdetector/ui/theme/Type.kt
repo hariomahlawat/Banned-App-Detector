2b025e056570c301ce6f72f0590d7ce5047e4fdd
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-07-20 21:06:53 +0530

    syntax error corrected

--- app/src/main/java/com/hariomahlawat/bannedappdetector/ui/theme/Type.kt
+++ app/src/main/java/com/hariomahlawat/bannedappdetector/ui/theme/Type.kt
@@ -2,14 +2,13 @@ package com.hariomahlawat.bannedappdetector.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.googlefonts.Font
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.googlefonts.GoogleFont.Provider
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.googlefonts.R
+import com.hariomahlawat.bannedappdetector.R
 
 private val provider = Provider(
     authority = "com.google.android.gms.fonts",
