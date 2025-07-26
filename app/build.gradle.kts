b515e62b7d797391328b018d1ad1e9e8cb62c3d3
Hari Om Ahlawat <hariomahlawat@gmail.com>
2025-07-26 18:33:14 +0530

    Version 2.0 - publish

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         minSdk = 28
         //noinspection EditedTargetSdkVersion
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
